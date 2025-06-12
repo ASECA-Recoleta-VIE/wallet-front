9f041da26b758399b1cea5b03ea35c7d02bcf704
Pablo <pablopagliaricci@gmail.com>
2025-06-12 01:07:06 -0300

    a

--- cypress/e2e/carga-saldo.cy.ts
+++ cypress/e2e/carga-saldo.cy.ts
@@ -1,67 +1,36 @@
 // Test E2E: Carga de saldo (Add Funds)
 // Asegura que un usuario pueda cargar saldo correctamente y que se muestren los mensajes de error si corresponde
 
-// Ignora errores uncaught del frontend para depuración
-Cypress.on('uncaught:exception', (err, runnable) => {
-  return false;
-});
+Cypress.on('uncaught:exception', () => false);
 
 describe('Carga de saldo', () => {
   beforeEach(() => {
     cy.clearCookies();
     cy.clearLocalStorage();
     cy.visit('/login');
+
     cy.document().its('readyState').should('eq', 'complete');
-    cy.get('body').should('contain.text', 'Login');
     cy.url().should('include', '/login');
+    cy.get('body').should('contain.text', 'Login');
 
-    cy.get('[data-testid="login-email"]', { timeout: 10000 })
-      .should('exist')
-      .should('be.visible')
-      .should('have.length', 1)
-      .then(($input) => {
-        cy.wrap($input).type('pablopagliaricci@gmail.com');
-      });
+    cy.get('[data-testid="login-email"]').should('be.visible').type('pablopagliaricci@gmail.com');
+    cy.get('[data-testid="login-password"]').should('be.visible').type('Password1!');
+    cy.get('[data-testid="login-submit"]').click();
 
-    cy.get('[data-testid="login-password"]', { timeout: 10000 })
-      .should('exist')
-      .should('be.visible')
-      .should('have.length', 1)
-      .then(($input) => {
-        cy.wrap($input).type('Password1!');
-      });
-
-    cy.get('[data-testid="login-submit"]', { timeout: 10000 }).click();
-
+    // Verificamos redirección a /wallet
     cy.url({ timeout: 10000 }).should('include', '/wallet');
-    cy.contains('Add Funds', { timeout: 10000 }).should('exist').should('be.visible');
-    cy.contains('Add Funds', { timeout: 10000 }).click();
+
+    // Accedemos a la funcionalidad de "Add Funds"
+    cy.contains('Add Funds', { timeout: 10000 }).should('be.visible').click();
   });
 
   it('permite cargar saldo correctamente', () => {
-    cy.get('[data-testid="addfunds-amount"]', { timeout: 10000 })
-      .should('exist')
-      .should('be.visible')
-      .should('have.length', 1)
-      .then(($input) => {
-        cy.wrap($input).type('100');
-      });
+    cy.get('[data-testid="addfunds-amount"]').type('100');
+    cy.get('[data-testid="addfunds-description"]').type('Recarga test');
+    cy.get('[data-testid="addfunds-submit"]').click();
 
-    cy.get('[data-testid="addfunds-description"]', { timeout: 10000 })
-      .should('exist')
-      .should('be.visible')
-      .should('have.length', 1)
-      .then(($input) => {
-        cy.wrap($input).type('Recarga test');
-      });
-
-    cy.get('[data-testid="addfunds-submit"]', { timeout: 10000 }).click();
-
-    // Verificar toast de éxito
     cy.contains('Funds added successfully!', { timeout: 10000 }).should('be.visible');
-
-    // Verificar que los campos se limpian
-    cy.get('[data-testid="addfunds-amount"]', { timeout: 10000 }).should('have.value', '');
-    cy.get('[data-testid="addfunds-description"]', { timeout: 10000 }).should('have.value', '');
+    cy.get('[data-testid="addfunds-amount"]').should('have.value', '');
+    cy.get('[data-testid="addfunds-description"]').should('have.value', '');
   });
 });
